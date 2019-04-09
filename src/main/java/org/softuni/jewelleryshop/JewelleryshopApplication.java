57244cdd482c4c66f1c7a4ade4f9a4e22a29cb2a
Plamen <plamen.borachev@gmail.com>
2019-04-09 19:37:51 +0300

    Initial commit

---
+++ src/main/java/org/softuni/jewelleryshop/JewelleryshopApplication.java
@@ -0,0 +1,13 @@
+package org.softuni.jewelleryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelleryshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JewelleryshopApplication.class, args);
+    }
+
+}
