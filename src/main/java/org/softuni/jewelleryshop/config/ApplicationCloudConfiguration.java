dd657a4441c833deee3e5faa8dcebf45152fd949
Plamen <plamen.borachev@gmail.com>
2019-04-09 19:59:17 +0300

    files added

--- src/main/java/org/softuni/jewelleryshop/config/ApplicationCloudConfiguration.java
+++ src/main/java/org/softuni/jewelleryshop/config/ApplicationCloudConfiguration.java
@@ -17,8 +17,6 @@ public class ApplicationCloudConfiguration {
     @Value("${cloudinary.api-secret}")
     private String cloudApiSecret;
 
-//    alabala
-
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(new HashMap<String, Object>(){{
