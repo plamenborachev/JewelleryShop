ca595ccca1ac3250afebda2a3bf68d47b3d0cd3b
Plamen <plamen.borachev@gmail.com>
2019-04-19 20:08:47 +0300

    head changed

--- src/main/java/org/softuni/jewelleryshop/web/controllers/GlobalExceptionHandler.java
+++ src/main/java/org/softuni/jewelleryshop/web/controllers/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package org.softuni.jewelleryshop.web.controllers;
 
-import org.softuni.jewelleryshop.web.annotations.PageTitle;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
